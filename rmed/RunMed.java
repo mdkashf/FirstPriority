a2fab1cd77b6e799896efb48a3355db1e2c8c577
Jessica Wu <jessicawu49260@gmail.com>
2018-05-17 23:52:52 -0400

    updated RunMed

--- rmed/RunMed.java
+++ rmed/RunMed.java
@@ -1,5 +1,22 @@
 /* RunMed Algo: 
 
+getMedian():
+Case 1 - If size of min and max heaps are equal
+Return the mean of the roots of the min and max heaps.
+Case 2 - If min heap is larger than max heap
+Return root of min heap
+Case 3 - If max heap is larger than min heap
+Return root of max heap.
+
+add(): 
+1. If the value is less than the root of the min heap, add it to the min heap
+If the value is greater than the root of the max heap, add it to the max heap
+
+2. To balance:
+If the min heap size is larger, add the value at the root to max heap and remove the root from min heap.
+If the max heap size is larger, add the value at the root to min heap and remove the root from max heap. 
+
+After all the values are added into their respective heap, then median can be found.
  */
 import java.util.NoSuchElementException;//in order to import exception
 public class RunMed {
